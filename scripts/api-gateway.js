29dad92cd6f92177f0c535b21ae20e50acea7467
crytome1995 <ethan.lebioda@gmail.com>
2021-05-04 21:07:16 -0500

    Added delete card api

--- scripts/api-gateway.js
+++ scripts/api-gateway.js
@@ -7,6 +7,7 @@ var POST_CARD_RESOURCE_PATH = "card";
 var GET_CARDS_RESOURCE_PATH = "card/";
 var POST = "POST";
 
+// return a json request representation for a card put request
 function getAddCardJSON(cardName) {
   var json = {
     [USER]: cache.getUserName(),
@@ -15,12 +16,14 @@ function getAddCardJSON(cardName) {
   return JSON.stringify(json);
 }
 
+// set client headers for interacting with the api gateway
 function setHeaders(client) {
   client.setRequestHeader("Content-type", "application/json");
   client.setRequestHeader("Authorization", cache.getIDToken());
   return client;
 }
 
+// add a card to the database
 export function addCard(cardName) {
   return new Promise(function (resolve, reject) {
     let url = GATEWAY_URL.concat(POST_CARD_RESOURCE_PATH);
@@ -40,6 +43,7 @@ export function addCard(cardName) {
   });
 }
 
+// return all cards that belong to the logged in user
 export function getCards(callback) {
   return new Promise(function (resolve, reject) {
     var cardResponse;
@@ -57,6 +61,33 @@ export function getCards(callback) {
         console.log("Got response: " + this.responseText);
       } else {
         console.log("Error: " + this);
+        reject();
+      }
+    };
+    client.send();
+  });
+}
+
+// delete a card from the database
+export function deleteCard(cardName) {
+  return new Promise(function (resolve, reject) {
+    let url = GATEWAY_URL.concat(
+      GET_CARDS_RESOURCE_PATH,
+      encodeURIComponent(cache.getUserName()),
+      "/",
+      cardName
+    );
+    let client = new XMLHttpRequest();
+    client.open(POST, url, false);
+    client = setHeaders(client);
+    client.onload = resolve;
+    client.onreadystatechange = function () {
+      // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        console.log("Got response: " + this.responseText);
+      } else {
+        console.log("Error: " + this);
+        reject();
       }
     };
     client.send();
