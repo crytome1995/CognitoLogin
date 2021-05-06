f8c66713c025b9c381f6a9de61be0ca414e15421
crytome1995 <ethan.lebioda@gmail.com>
2021-05-05 21:34:41 -0500

    Add checkboxelement class to help with element

--- scripts/util.js
+++ scripts/util.js
@@ -7,3 +7,16 @@ export function validateCardName(cardName) {
     return false;
   }
 }
+
+// helper class to set select checkbox to selected and not selected
+export class CheckBoxElement {
+  constructor() {}
+
+  get checked() {
+    return '<input type="checkbox" checked="true" id="transactionCheckBox">';
+  }
+
+  get unchecked() {
+    return '<input type="checkbox" id="transactionCheckBox">';
+  }
+}
